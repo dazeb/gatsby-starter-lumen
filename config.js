a9883493a4edf52c82c367a531580998ea75fddc
Darren Bennett <dbennett2911@gmail.com>
2021-09-25 20:59:19 +0100

    add pic

--- config.js
+++ config.js
@@ -3,9 +3,9 @@
 module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
+  title: 'Blog by Darren Bennett',
+  subtitle: 'Currently learning React, Vue & Django',
+  copyright: 'blog.dazeb.uk © All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
@@ -25,9 +25,9 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Darren Bennett',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Currently learning React, Vue & Django',
     contacts: {
       email: '',
       facebook: '#',
